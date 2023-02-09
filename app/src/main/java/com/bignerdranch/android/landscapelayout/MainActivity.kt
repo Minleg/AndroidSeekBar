e071acf1b563160df92db706047146eb15436fbe
minleg <minleg@minleg.com>
2023-02-09 11:22:08 -0600

    Starting SquareActivity

--- app/src/main/java/com/bignerdranch/android/landscapelayout/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/landscapelayout/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.bignerdranch.android.landscapelayout
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+
+const val EXTRA_SQUARE_SIZE = "com.bignerdranch.android.landscapelayout.tap_the_square.SQUARE_SIZE"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var seekBar: SeekBar
     private lateinit var seekBarLabel: TextView
+    private lateinit var showSquareButton: Button
+
+    private val squareResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            handleSquareResult(result)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +29,10 @@ class MainActivity : AppCompatActivity() {
 
         seekBar = findViewById(R.id.seek_bar)
         seekBarLabel = findViewById(R.id.seek_bar_label)
+        showSquareButton = findViewById(R.id.show_square_button)
 
-        val initialProgress = seekBar.progress + 1 // to set seekbar range from 1 instead of default 0
+        val initialProgress =
+            seekBar.progress + 1 // to set seekbar range from 1 instead of default 0
         updateLabel(initialProgress)
 
         // TODO add listener to update label as seekbar changes
@@ -33,9 +48,41 @@ class MainActivity : AppCompatActivity() {
             // when user stops dragging
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
+
+        showSquareButton.setOnClickListener {
+            showSquare()
+        }
     }
 
     private fun updateLabel(progress: Int) {
         seekBarLabel.text = getString(R.string.seekbar_value_message, progress)
+    }
+
+    private fun showSquare() {
+        // launch the SquareActivity
+        val showSquareIntent = Intent(this, SquareActivity::class.java)
+
+        // Tell the SquareActivity how large the square should be
+        // based on the progress of the Seekbar.
+        showSquareIntent.putExtra(EXTRA_SQUARE_SIZE, seekBar.progress)
+        // startActivity(showSquareIntent) // starts the SquareActivity but there is no way to receive data back from SquareActivity
+        squareResultLauncher.launch(showSquareIntent)
+    }
+    private fun handleSquareResult(result: ActivityResult) {
+        // TODO display result to user
+        if (result.resultCode == RESULT_OK) {
+            val intent = result.data
+            // if intent is null, tapped is false because of Elvis operator false ( second false)
+            // if EXTRA... doesn't have any value, then is set to false (first false)
+            val tapped = intent?.getBooleanExtra(EXTRA_TAPPED_INSIDE_SQUARE, false) ?: false // non nullable- used elvis operator
+            val message = if (tapped) {
+                getString(R.string.tapped_square)
+            } else {
+                getString(R.string.missed_square)
+            }
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        } else if (result.resultCode == RESULT_CANCELED) {
+            Toast.makeText(this, getString(R.string.did_not_try), Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/landscapelayout/SquareActivity.kt
@@ -0,0 +1,52 @@
+package com.bignerdranch.android.landscapelayout
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+const val EXTRA_TAPPED_INSIDE_SQUARE =
+    "com.bignerdranch.android.landscapelayout.TAPPED_INSIDE_SQUARE"
+
+class SquareActivity : AppCompatActivity() {
+
+    private lateinit var squareImage: ImageView
+    private lateinit var container: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_square)
+
+        squareImage = findViewById(R.id.square)
+        container = findViewById(R.id.container)
+
+        var squareSize = intent.getIntExtra(EXTRA_SQUARE_SIZE, 100)
+        if (squareSize == 0) {
+            squareSize = 1
+        }
+
+        squareImage.layoutParams.width = squareSize
+        squareImage.layoutParams.height = squareSize
+
+        squareImage.setOnClickListener {
+            squareTapped(true)
+        }
+
+        container.setOnClickListener {
+            squareTapped(false)
+        }
+    }
+
+    private fun squareTapped(didTapSqaure: Boolean) {
+        // todo send a message to main activity
+        // end square activity
+        // Toast.makeText(this, "square tapped $didTapSqaure", Toast.LENGTH_SHORT).show()
+        val resultIntent = Intent()
+        resultIntent.putExtra(EXTRA_TAPPED_INSIDE_SQUARE, didTapSqaure)
+        // if user clicked back button before doing the required task in the activity,
+        // it would be RESULT_CANCEL
+        setResult(RESULT_OK, resultIntent) // RESULT_OK indicates that everything worked as intented and user tapped on somewhere
+        finish() // finishing the SquareActivity and we return back to calling Activity which is the MainActivity
+    }
+}
